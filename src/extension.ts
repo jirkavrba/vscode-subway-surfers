821cf0974ddae4572cbe0562e851b87c796038f7
lg <abbix@riseup.net>
2023-05-01 08:01:26 -0400

    Add support for minecraft

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,73 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-const videos = [
-	"nNGQ7kMhGuQ",
-	"Tqne5J7XdPA",
-	"hs7Z0JUgDeA",
-	"iYgYfHb8gbQ"
+const subway_videos = [
+  "nNGQ7kMhGuQ",
+  "Tqne5J7XdPA",
+  "hs7Z0JUgDeA",
+  "iYgYfHb8gbQ",
 ];
 
+const minecraft_videos = [
+  "intRX7BRA90",
+  "n_Dv4JMiwK8",
+  "GTaXbH6iSFA",
+  "t3SpmH9QQew",
+];
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "subway-surfers.overstimulate",
+    () => {
+      const column = {
+        viewColumn: vscode.ViewColumn.Beside,
+        preserverFocus: true,
+      };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('subway-surfers.overstimulate', () => {
-		const column = { viewColumn: vscode.ViewColumn.Beside, preserverFocus: true };
-		const options = { enableScripts: true };
+      const options = { enableScripts: true };
 
-		const panel = vscode.window.createWebviewPanel('subway-surfers.video', "This code boring ah hell", column, options);
-		const video = videos.sort(() => Math.random() - 0.5)[0];
+      const panel = vscode.window.createWebviewPanel(
+        "subway-surfers.video",
+        "This code boring ah hell",
+        column,
+        options
+      );
 
-		panel.reveal();
-		panel.webview.html = `
+      let items: vscode.QuickPickItem[] = [
+        {
+          label: "Subway Surfers",
+        },
+        {
+          label: "Minecraft Parkour",
+        },
+      ];
+
+      let videos = subway_videos;
+      let width = 300;
+
+      vscode.window
+        .showQuickPick(items, {
+          placeHolder: "Choose your overstimulation method",
+        })
+        .then((selection) => {
+          if (!selection) return;
+
+          if (selection.label == "Subway Surfers") {
+            videos = subway_videos;
+          } else if (selection.label == "Minecraft Parkour") {
+            videos = minecraft_videos;
+            width = 600;
+          }
+
+          const video = videos.sort(() => Math.random() - 0.5)[0];
+
+          panel.reveal();
+          panel.webview.html = `
             <html lang="en"> 
                 <head>
                     <meta charset="utf-8"/>
@@ -42,17 +85,19 @@ export function activate(context: vscode.ExtensionContext) {
                 </head>
                 <body>
                     <div id="video">
-						<video autoplay muted controls width="300">
+						<video autoplay muted controls width="${width}">
 							<source src="https://yewtu.be/latest_version?id=${video}&amp;itag=22#t=100">
 						</video>
 					</div>
                 </body>
             </html>
         `;
-	});
+        });
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}

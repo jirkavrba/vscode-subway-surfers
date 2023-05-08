f2483096347943855d444c46ebf0ecb6000c5a93
Aaron vandenberg <57527473+aaronvandenberg@users.noreply.github.com>
2023-05-08 14:35:03 +0200

    -

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 type VideoSource = {
@@ -7,6 +5,12 @@ type VideoSource = {
     videos: string[];
     width: number;
 };
+
+type AudioSource = {
+    label: string;
+    audios: string[];
+};
+
 
 const internalVideoSources: VideoSource[] = [
     {
@@ -25,20 +29,31 @@ const internalVideoSources: VideoSource[] = [
         width: 600,
     },
     {
-      label: "Better Call Saul Clips",
-      videos: ["P0Gl0Sd7K3k", "ySs3T3tc_bQ", "XQQI72wQjEA", "gsAeYmTNL80"],
-      width: 600
+        label: "Better Call Saul Clips",
+        videos: ["P0Gl0Sd7K3k", "ySs3T3tc_bQ", "XQQI72wQjEA", "gsAeYmTNL80"],
+        width: 600
     },
     {
         label: "Mental Outlaw",
         videos: ["Sk2O6aOEPLM", "Lk_v6Q0YsNo", "3oPeIbpA5x8", "GR_U0G-QGA0"],
         width: 600
-      },
-      {
-        label: "Industrial Society and its Future",
-        videos: ["IIcGyMwKgqQ"],
-        width: 600
-      }
+    },
+
+];
+
+const internalAudioSources: AudioSource[] = [
+    {
+        label: "Industrial Society And Its Future",
+        audios: ["https://ia600108.us.archive.org/1/items/TheodoreJohnKaczynskiIndustrialSocietyAndItsFuture1995www.MP3Fiber.com/Theodore_John_Kaczynski_Industrial_Society_and_Its_Future_1995%5Bwww.MP3Fiber.com%5D.mp3"],
+    },
+    {
+        label: "Calm",
+        audios: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"],
+    },
+    {
+        label: "Lively",
+        audios: ["https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"],
+    },
 ];
 
 // This method is called when your extension is activated
@@ -46,9 +61,10 @@ const internalVideoSources: VideoSource[] = [
 export function activate(context: vscode.ExtensionContext) {
     const userVideoSources: VideoSource[] = vscode.workspace.getConfiguration().get('subway-surfers.customSources') || [];
     const videoSources = internalVideoSources.concat(userVideoSources);
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
+
+    const userAudioSources: AudioSource[] = vscode.workspace.getConfiguration().get('subway-surfers.customAudioSources') || [];
+    const audioSources = internalAudioSources.concat(userAudioSources);
+
     let disposable = vscode.commands.registerCommand("subway-surfers.overstimulate", () => {
         const column = {
             viewColumn: vscode.ViewColumn.Beside,
@@ -57,59 +73,99 @@ export function activate(context: vscode.ExtensionContext) {
 
         const options = { enableScripts: true };
 
-        const panel = vscode.window.createWebviewPanel(
-            "subway-surfers.video",
-            "This code boring ah hell",
-            column,
-            options
-        );
-
-        const items: vscode.QuickPickItem[] = videoSources.map((source) => {
+        const panel = vscode
+        const mediaItems: vscode.QuickPickItem[] = videoSources.map((source) => {
             return {
                 label: source.label,
+                detail: "Video",
                 alwaysShow: true,
             };
-        });
-
-        vscode.window.showQuickPick(items, { placeHolder: "Choose your overstimulation method" }).then((selection) => {
+        }).concat(audioSources.map((source) => {
+            return {
+                label: source.label,
+                detail: "Audio",
+                alwaysShow: true,
+            };
+        }));
+    
+        vscode.window.showQuickPick(mediaItems, { placeHolder: "Choose your overstimulation method" }).then((selection) => {
             if (!selection) {
                 return;
             }
+            if (selection.detail === "Video") {
+                const selectedSource = videoSources.find((source) => source.label === selection.label);
+                if (!selectedSource) {
+                    return;
+                }
+                const { videos, width } = selectedSource;
+                const video = videos.sort(() => Math.random() - 0.5)[0];
+                const panel = vscode.window.createWebviewPanel(
+                    "subwaySurfers", // Identifies the type of the webview
+                    "Subway Surfers", // Title of the panel displayed to the user
+                    column, // Editor column to show the new webview panel in
+                    options // Webview options
+                );
 
-            const { videos, width } = videoSources.find((source) => source.label === selection.label)!;
-            const video = videos.sort(() => Math.random() - 0.5)[0];
-
-            panel.reveal();
-            panel.webview.html = `
-            <html lang="en"> 
-                <head>
-                    <meta charset="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <style>
-                        #video {
-                            display: flex;
-                            flex-flow: column nowrap;
-                            justify-content: center;
-                            align-items: center;
-                            width: 100%;
-                            height: 100%;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div id="video">
-						<video autoplay controls width="${width}">
-							<source src="https://yewtu.be/latest_version?id=${video}&amp;itag=22#t=100">
-						</video>
-					</div>
-                </body>
-            </html>
-        `;
+                panel.reveal();
+                panel.webview.html = `
+                    <html lang="en"> 
+                        <head>
+                            <meta charset="utf-8"/>
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <style>
+                                #video {
+                                    display: flex;
+                                    flex-flow: column nowrap;
+                                    justify-content: center;
+                                    align-items: center;
+                                    width: 100%;
+                                    height: 100%;
+                                }
+                            </style>
+                        </head>
+                        <body>
+                            <div id="video">
+                                <video autoplay controls width="${width}">
+                                    <source src="https://yewtu.be/latest_version?id=${video}&amp;itag=22#t=100">
+                                </video>
+                            </div>
+                        </body>
+                    </html>
+                `;
+            } else if (selection.detail === "Audio") {
+                const selectedSource = audioSources.find((source) => source.label === selection.label);
+                if (!selectedSource) {
+                    return;
+                }
+                const { audios } = selectedSource;
+                const audio = audios.sort(() => Math.random() - 0.5)[0];
+                const panel = vscode.window.createWebviewPanel(
+                    "subwaySurfers", // Identifies the type of the webview
+                    "Subway Surfers", // Title of the panel displayed to the user
+                    column, // Editor column to show the new webview panel in
+                    options // Webview options
+                );
+                panel.reveal();
+                panel.webview.html = `
+         <html lang="en"> 
+               <head>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
+            <body>
+                <div>
+                    <audio controls>
+                        <source src="${audio}">
+                    </audio>
+                </div>
+            </body>
+        </html>
+    `;
+            }
         });
     });
-
+    
     context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  }
